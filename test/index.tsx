1c593ce11a8ed5aa4d8518ef6af9788aa541603c
fimars <foru.fy@gmail.com>
2018-09-21 14:09:02 +0800

    done, the point is declare module

---
+++ test/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App from '../README.md';
+
+const rootEl = document.querySelector('div');
+
+ReactDOM.render(<App />, rootEl);
